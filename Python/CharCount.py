4cd4bcab800eed5eae30b9f30da8bd31b24a61f8
davidbaines <david_baines@sil.org>
2018-02-20 22:51:40 +0000

    Working code.
    This version shows the unicode name of every character.

--- Python/CharCount.py
+++ Python/CharCount.py
@@ -1,31 +1,30 @@
-
 from collections import Counter as Counter
-import argparse
+import argparse, io
+import unicodedata
 
-parser = argparse.ArgumentParser(description="Count the occurances of Characters in a file.")
+parser = argparse.ArgumentParser(description="Count occurances of all characters in a file.")
 parser.add_argument("-in", "--input",   help="Specify file to read in.")
-parser.add_argument("-out", "--output",   help="Specify file to write.")
+parser.add_argument("-out" ,"--output", help="Specify the filename for the report.")
 args = parser.parse_args()
 
-charcount = Counter()
+char_count = Counter()
+
+with io.open(args.input, 'r', encoding='utf-8') as infile:
+	for line in infile:
+		char_count.update(line)
+
+header_string = "{0:>10s} | {1:<s} | {2:}"
+row_string = "{0:>10d} | {1:<s} | {2:}"
+column1 = "Number"
+column2 = "Character"
+column3 = "Unicode name"
+
+print(header_string.format(column1,column2, column3))
+
+for character,count in char_count.most_common():
+	try:
+		name = unicodedata.name(character)
+	except:
+		name = ""
+	print(row_string.format(count,character, name))
 	
-if not args.input:
-	print("Use: CharCount -in <inputfile>")
-else :
-	with open(args.input, 'r', encoding='utf-8') as infile:
-		for line in (infile):
-			for character in line.strip('()[]0123456789\n'):
-				charcount.update(character)
-
-if args.output:
-	with open(args.output, 'w', encoding='utf-8') as outfile:
-		for character in charcount:
-			outfile.write(character)
-
-print()
-for character in charcount.most_common():
-	print(character)
-print()
-
-for character in charcount.most_common():				
-	print(" {}  ,  {} ".format([character],charcount[character]))
