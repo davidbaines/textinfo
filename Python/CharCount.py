94fdae16566ff1026432a0cd862037981775d2c5
davidbaines <david_baines@sil.org>
2018-02-21 11:22:08 +0000

    Removed the newlines
    Removed the newlines from the output to tidy up the table. Tried to adapt the table for multi byte characters with limited sucess.

--- Python/CharCount.py
+++ Python/CharCount.py
@@ -2,12 +2,21 @@ from collections import Counter as Counter
 import argparse, io
 import unicodedata
 
-parser = argparse.ArgumentParser(description="Count occurances of all characters in a file.")
+parser = argparse.ArgumentParser(description="Count occurrences of all characters in a file.")
 parser.add_argument("-in", "--input",   help="Specify file to read in.")
-parser.add_argument("-out" ,"--output", help="Specify the filename for the report.")
+parser.add_argument("-out" ,"--output", help="Specify the file name for the report.")
 args = parser.parse_args()
 
+column_names = "{0:>10s}  {1:<9} {2:}".format("Number","Character","Unicode name")
+row_string_one_byte   = "{0:>10d} | {1:<9} |{2:}"
+row_string_more_bytes = "{0:>10d} | {1:<9}|{2:}"
+
+def count_bytes(character):
+	return len(character.encode('utf8'))
+
 char_count = Counter()
+char_data = []
+
 if not args.input:
 	print("Use: CharCount -in <inputfile>")
 	exit()
@@ -16,27 +25,30 @@ else:
 		for line in infile:
 			char_count.update(line)
 
-header_string = "{0:>10s} | {1:<s} | {2:}"
-row_string = "{0:>10d} | {1:<s} | {2:}"
-column1 = "Number"
-column2 = "Character"
-column3 = "Unicode name"
-
-print(header_string.format(column1,column2, column3))
+#Delete newline count.
+del char_count['\n']
 
 for character,count in char_count.most_common():
 	try:
 		name = unicodedata.name(character)
 	except:
-		name = ""
-	print(row_string.format(count,character, name))
+		name = "Couldn't find Unicode name"
+	char_data.append((count,character,name))
 
+print(column_names)
+
+for count,character, name in char_data:
+	if count_bytes(character) == 1 :
+		print(row_string_one_byte.format(count,character, name))
+	else :
+		print(row_string_more_bytes.format(count,character, name))
+		
 if args.output:
 	with open(args.output, 'w', encoding='utf-8') as outfile:
-		for character,count in char_count.most_common():
-			try:
-				name = unicodedata.name(character)
-			except:
-				name = ""
-			outfile.write(row_string.format(count,character, name))
+		outfile.write(column_names + '\n')
+		for count, character, name in char_data:
+			if count_bytes(character) == 1 :
+				outfile.write(row_string_one_byte.format(count,character, name))
+			else : 
+				outfile.write(row_string_more_bytes.format(count,character, name))
 			outfile.write("\n")
