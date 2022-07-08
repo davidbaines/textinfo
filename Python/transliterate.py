8f113f4bb805b79fb0bb96cdb257b51f5ca23d1c
David Baines <david_baines@sil.org>
2022-07-08 12:01:13 +0100

    Add transliterate.py - use to remove certain characters that inherit their script.

---
+++ Python/transliterate.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import argparse
+import unicodedata
+from pathlib import Path
+import string
+
+"""
+Transliterate:
+ARABIC FATHATAN
+ARABIC DAMMATAN
+ARABIC KASRATAN
+ARABIC FATHA
+ARABIC DAMMA
+ARABIC KASRA
+ARABIC SHADDA
+ARABIC SUKUN
+ARABIC HAMZA ABOVE
+ARABIC LETTER SUPERSCRIPT ALEF
+ZERO WIDTH NON JOINER           
+ZERO WIDTH JOINER             
+
+to:
+
+ARABIC LETTER ALEF WITH EXTENDED ARABIC-INDIC DIGIT TWO ABOVE
+ARABIC LETTER WAW WITH EXTENDED ARABIC-INDIC DIGIT TWO ABOVE
+ARABIC LETTER FARSI YEH WITH EXTENDED ARABIC-INDIC DIGIT TWO ABOVE
+ARABIC LETTER YU
+ARABIC LETTER OE
+ARABIC LETTER U
+ARABIC LETTER TAH WITH THREE DOTS BELOW
+ARABIC LETTER TAH WITH DOT BELOW
+ARABIC TATWEEL WITH OVERSTRUCK HAMZA
+ARABIC LETTER LOW ALEF
+ARABIC TATWEEL WITH OVERSTRUCK WAW
+ARABIC TATWEEL WITH TWO DOTS BELOW
+
+"""
+
+source          = [1611,1612,1613,1614,1615,1616,1617,1618,1620,1648,8204,8205]
+destination     = [1907,1912,1909,1736,1734,1742,2188,2187,2179,2221,2180,2181]
+transliteration = {s:d for s,d in zip(source, destination)}
+  
+def main():
+
+    parser = argparse.ArgumentParser(description="Write csv reports about the characters found in multiple files.")
+    parser.add_argument('input_file',  type=Path, help="Input file to transliterate.")
+    parser.add_argument('--output_file', type=Path, help="Output file.")
+    
+    args = parser.parse_args()
+    input_file = args.input_file
+    
+        
+#    print(input_file)  
+#    print(output_file)
+    
+    with open(input_file, 'r', encoding='utf-8', newline='') as file_in:
+        if args.output_file:
+            output_file = Path(args.output_file)
+            with open(output_file, 'w', encoding='utf-8', newline='') as file_out:
+                for line in file_in:
+                    file_out.write(line.translate(transliteration))
+        else:
+            for line in file_in:
+                print(line.translate(transliteration))
+                
+#        print(output_line)
+#        if output_line != line:
+#            print(line)
+#            print(output_line)
+    
+if __name__ == "__main__":
+    main()
