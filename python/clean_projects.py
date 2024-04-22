6dcbe5b0daf8e85e8a1b04fe8f694a47dd46caeb
David Baines <david_baines@sil.org>
2024-04-22 15:54:41 +0200

    Update clean_projects.py

--- python/clean_projects.py
+++ python/clean_projects.py
@@ -2,58 +2,43 @@ from pathlib import Path
 import shutil
 import argparse
 
-def get_files_to_copy(project):
-    
-    files = [file for file in project.glob('*') if file.is_file]
+def get_sub_folders(folder):
+    return [f for f in folder.glob("*") if f.is_dir()]
+ 
 
-    # Only copy if there are sfm files.
-    files_to_copy = [file for file in files if file.suffix.lower() in ['.usfm','.sfm']]
-    if not files_to_copy:
-        return None
-    
-    files_to_copy.extend([file for file in files if file.name.lower() in ['settings.xml', 'custom.sty'] or file.suffix.lower() == '.ldml' ])
-    return files_to_copy
+def choose_yes_no(prompt: str) -> bool:
+
+    choice: str = " "
+    while choice not in ["n","y"]:
+        choice: str = input(prompt).strip()[0].lower()
+    if choice == "y":
+        return True
+    elif choice == "n":
+        return False
+
 
 def main():
     
-    parser = argparse.ArgumentParser(description="Clean Paratext project prior to uploading.")
-    parser.add_argument('input',  type=Path, help="Folder to search")
-    parser.add_argument('--output', type=Path, help="Folder for the output results.", required=False)
+    parser = argparse.ArgumentParser(description="Clean Paratext projects.")
+    parser.add_argument('--input',  type=Path, default = Path("S:/Paratext/projects"), help="Folder to search")
     args = parser.parse_args()
     
     input = Path(args.input)
-    #print(args.output)
- 
-    default_output = input.parent / "cleaned_projects"
-    if args.output:
-        output = Path(args.output)
-    else:
-        output = default_output
 
-    #print(input.parent, type(input.parent))
-    print(f"Will write cleaned project folders to {output}")
-    output.mkdir(parents=True, exist_ok=True)
+    projects = get_sub_folders(input)
 
-    projects = [folder for folder in input.glob('*') if folder.is_dir()]
-    #for project in projects:
-        #print(project)
-    #print()
-
+    
     for project in projects:
-        files_to_copy = get_files_to_copy(project)
-        #print(f"Looking in {project}")
-        if files_to_copy:
-            copy_to_folder = output / files_to_copy[0].parent.name
-            print(f"Copying files to {copy_to_folder}")
-            copy_to_folder.mkdir(parents=True, exist_ok=True)
+        sub_folders = get_sub_folders(project)
+        files_to_delete  = [ f for f in project.glob("*") if f.is_file() and any(string in f.name for string in ["Notes", "unique", "licence", "license", "backup", "Print", "User" "Access"]) ]
+        if sub_folders or files_to_delete:
+            if True: #choose_yes_no(f"Delete unnecessary files and folders from {project}?"):
+                for sub_folder in sub_folders:
+                    shutil.rmtree(sub_folder)
+                    print(f"Attempted to delete {sub_folder}")
+                for file_to_delete in files_to_delete:
+                    file_to_delete.unlink()
+                    print(f"Attempted to delete {file_to_delete}")
 
-            for file_to_copy in files_to_copy:
-                dest_file = copy_to_folder / file_to_copy.name
-
-                # Write an exact copy of the file from the source to the new destination.    
-                shutil.copyfile(file_to_copy, dest_file) 
-                #print(f"Writing:  {dest_file}")
-        else:
-            print(f"No sfm files found in {project}")
 if __name__ == "__main__":
     main()
